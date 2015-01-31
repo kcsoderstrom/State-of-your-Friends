9edf6eb0a1cf4222a5a8b011dddc5be415af3753
KC Soderstrom <jacquelinesoderstrom@gmail.com>
2015-01-31 15:22:49 -0500

    Move control to the router

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -9,4 +9,18 @@ class SurveysController < ApplicationController
 											.where("user_survey_questions.is_deleted = FALSE")
 		render :show
 	end
+
+	def swap_current_question
+		question = Question.find(params[:id])
+		if question && current_user.current_question_id != question.id
+			current_user.update_attribute(:current_question_id, question.id)
+		end
+
+		@survey = Question.includes(:choices)
+									.includes(:responses)
+									.joins(:user_survey_questions)
+									.where("user_survey_questions.user_id = #{current_user.id}")
+									.where("user_survey_questions.is_deleted = FALSE")
+		render :show
+	end
 end
