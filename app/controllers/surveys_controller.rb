76cfb0796bf130d524c63604d376788d9ef0d697
KC Soderstrom <jacquelinesoderstrom@gmail.com>
2015-02-07 17:42:55 -0500

    Redirect from survey url to current question's survey url

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,12 +2,7 @@ class SurveysController < ApplicationController
 	before_action :require_current_user
 
 	def show
-		@survey = Question.includes(:choices)
-											.includes(:responses)
-											.joins(:user_survey_questions)
-											.where("user_survey_questions.user_id = #{current_user.id}")
-											.where("user_survey_questions.is_deleted = FALSE")
-		render :show
+		redirect_to survey_url(current_user.current_question_id)
 	end
 
 	def swap_current_question
