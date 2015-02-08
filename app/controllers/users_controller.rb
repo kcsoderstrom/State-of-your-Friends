fecf7004c6b59dfa706b73856f965ccc57be349b
KC Soderstrom <jacquelinesoderstrom@gmail.com>
2015-02-07 19:10:34 -0500

    Fix typo in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	def update
 		@user = current_user
 		if @user.update(params.permit(:current_question_id))
-			render json: { current_question_id: @user.curent_question_id,
+			render json: { current_question_id: @user.current_question_id,
 										 previous_question_count: @user.previous_questions.count,
 										 next_question_count: @user.next_questions.count }
 		else
