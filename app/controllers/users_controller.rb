4066162efadf1352b977cc9bfa55b1da4adf9ea8
KC Soderstrom <jacquelinesoderstrom@gmail.com>
2015-02-07 21:45:47 -0500

    Fix question delete bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	def update
 		@user = current_user
-		if @user.update(params.permit(:current_question_id)) || @user.delete_question(params[:deleted_question_id])
+		if (params[:current_question_id] && @user.update(params.permit(:current_question_id))) || @user.delete_question(params[:deleted_question_id])
 			render json: { current_question_id: @user.current_question_id,
 										 previous_question_count: @user.previous_questions.count,
 										 next_question_count: @user.next_questions.count }
