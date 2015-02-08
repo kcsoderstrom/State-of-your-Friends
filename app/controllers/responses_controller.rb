81ae0dce3cee804ad69f20976201acdb3f3c0287
KC Soderstrom <jacquelinesoderstrom@gmail.com>
2015-02-07 19:50:40 -0500

    Update choices on click

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,15 +1,7 @@
 class ResponsesController < ApplicationController
 	def create
-		response_params[:choice_ids].each do |choice_id|
-			next unless choice_id.present?
-			response = Response.new(choice_id: choice_id, user_id: current_user.id)
-			response.displace_current_response
-		end
+		response = Response.new(choice_id: params[:choice_id], user_id: current_user.id)
+		response.displace_current_response
 		redirect_to survey_url
-	end
-
-	private
-	def response_params
-		params.require(:response).permit(choice_ids: [])
 	end
 end
